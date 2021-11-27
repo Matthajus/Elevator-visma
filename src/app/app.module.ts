d9305f0fede9d6d0c0e0a3837f5ec2710da7ee78
Matúš Paľo <matus.palo@student.upjs.sk>
2021-11-27 19:22:28 +0100

    Modify model + add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ElevatorComponent } from './components/elevator/elevator.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from "@angular/material/slider";
+import {MatIconModule} from "@angular/material/icon";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -12,7 +17,12 @@ import { ElevatorComponent } from './components/elevator/elevator.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models/elevator-class.ts
+++ src/app/models/elevator-class.ts
@@ -2,9 +2,8 @@ import {ElevatorDirectionEnum} from "./elevator-direction-enum";
 
 export class ElevatorClass {
 
-  currentFloor?: number;
-  direction?: ElevatorDirectionEnum;
-  moveUpQueue?: number[];
-  moveDownQueue?: number[];
+  currentFloor: number = 0;
+  direction: ElevatorDirectionEnum = ElevatorDirectionEnum.MOVE_UP;
+  moveQueue = Array<number>();
 
 }
